8c19443cde265fc9227797c8bf088e2dfd585866
Kyle d'Oliveira <kyle.doliveira@clio.com>
2019-11-18 12:20:48 -0800

    Anonymous comes from an active_support extension and the method is trivial so we don't need to introduce the same dependency into the gem

--- lib/dead_code_detector/initializer.rb
+++ lib/dead_code_detector/initializer.rb
@@ -66,7 +66,7 @@ module DeadCodeDetector
       private
       def descendants_of(parent_class)
         ObjectSpace.each_object(parent_class.singleton_class).select do |klass|
-          klass < parent_class && !klass.anonymous?
+          klass < parent_class && !klass.name.nil?
         end
       end
 
